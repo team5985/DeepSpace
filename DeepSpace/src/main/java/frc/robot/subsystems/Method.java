e28d89f6666702507bac0913eda368def4d05480
LordBBQ <46541720+LordBBQ@users.noreply.github.com>
2019-01-17 15:36:05 +1100

    New class for general muli-purpose methods.
    New method for interupting via violent movement of the stick.

---
+++ DeepSpace/src/main/java/frc/robot/subsystems/Method.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public abstract class Method {
+	
+	public boolean methodStickInterupt() {
+		boolean end = false;
+		Stick = new Joystick(1);
+
+		if(Stick.getX >= 0.7) {
+			end = true;
+		}else if(Stick.getX <= -0.7) {
+			end = true;
+		}else if(Stick.getY >= 0.7) {
+			end = true;
+		}else if(Stick.getY <= -0.7) {
+			end = true;
+		}else if(Stick.getZ >= 0.7) {
+			end = true;
+		}else if(Stick.getZ <= -0.7) {
+			end = true;
+		}
+	
+	return end;
+	}
+
+
+
+
+}
