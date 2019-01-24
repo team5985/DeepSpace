8e26cebf0030be7f072db8444d5168bd6db55cd9
LordBBQ <46541720+LordBBQ@users.noreply.github.com>
2019-01-24 15:35:28 +1100

    Moved code as requested

--- DeepSpace/src/main/java/frc/robot/TeleopController.java
+++ DeepSpace/src/main/java/frc/robot/TeleopController.java
@@ -1,12 +1,15 @@
 package frc.robot;
 
 import frc.robot.DriverControls;
-import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Drive; 
+
+
 
 /**
  * Manager of robot controls and movement during teleop period.
  */
 public class TeleopController {
+    boolean hatchMode = true;
     public static TeleopController teleopInstance = null;
 
     public TeleopController getInstance() {
@@ -28,5 +31,16 @@ public class TeleopController {
      */
     public void runTeleop() {
         _drive.arcadeDrive(_controls.getDrivePower(), _controls.getDriveSteering(), _controls.getDriveThrottle());
+
+        if (DriverControls.getButtonPress3()) {
+            hatchMode = true;
+          }else if (DriverConrols.getRawButtonPress4()) {
+            hatchMode = false;
+          }
+
     }
+
+   
+    
+
 }
